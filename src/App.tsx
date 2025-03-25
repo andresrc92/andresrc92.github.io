18fd331ea7c021f03befe7ab8d520e1e96792c20
andresrc92 <andresrivcoi@gmail.com>
2025-03-25 14:11:55 -0300

    checking deployment

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        {/* <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
@@ -24,7 +24,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-      </div>
+      </div> */}
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
